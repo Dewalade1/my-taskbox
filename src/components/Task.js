82f8e47abf64f6f1578e178726dbf6f8c7810b74
Dewa <adewalade@gmail.com>
2021-05-06 22:15:35 +0100

    chore: add story for long name edge case

--- src/components/Task.js
+++ src/components/Task.js
@@ -9,7 +9,7 @@ export default function Task ({ task: {id, title, state}, onArchiveTask, onPinTa
           <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
         </label>
         <div className="title">
-          <input type="text" value={title} readOnly={true} placeholder="Input Task title" style={{ color: "#2cc5d2" }} />
+          <input type="text" value={title} readOnly={true} placeholder="Input Task title" style={{ color: "#2cc5d2", textOverflow: "Ellipsis" }} />
         </div>
 
         <div className="actions" onClick={(event) => event.stopPropagation()}>
